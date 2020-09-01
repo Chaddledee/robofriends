8683327226cfbc9d31d8cdcca2472331c2b28eac
Charlie Tanner <charles.c.tanner@googlemail.com>
2020-09-01 19:31:25 +0100

    Implement Redux for state management.

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,49 +1,50 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import './App.css';
 import Scroll from '../components/Scroll';
 
+import { setSearchField, requestRobots } from '../actions.js'
+
+const mapStateToProps = (state) => {
+	return {
+		searchField: state.searchRobots.searchField,
+		robots: state.requestRobots.robots,
+		isPending: state.requestRobots.isPending,
+		error: state.requestRobots.error
+	}
+}
+
+const mapDispatchToProps = (dispatch) => {
+	return {
+		onSearchChange: (event) => dispatch(setSearchField(event.target.value)),
+		onRequestRobots: () => dispatch(requestRobots())
+	}
+}
+
 class App extends React.Component {
-	constructor() {
-		super();
-		this.state = {
-			robots: [],
-			searchfield: ''
-		};
-	}
-
-	onSearchChange = (event) => {
-		this.setState({searchfield: event.target.value});
-	}
-
 	componentDidMount() {
-		fetch('https://jsonplaceholder.typicode.com/users')
-			.then(res => res.json())
-			.then(users => {
-				this.setState({ robots: users });
-			})
+		this.props.onRequestRobots();
 	}
 
 	render() {
-		const filteredRobots = this.state.robots.filter(robot => {
-			return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase())
-		})
-		if (this.state.robots.length === 0) {
-			return <h1 className='tc'>LOADING</h1>
-		}
-		else {
-			return (
+		const { searchField, onSearchChange, robots, isPending } = this.props;
+		const filteredRobots = robots.filter(robot => {
+			return robot.name.toLowerCase().includes(searchField.toLowerCase())
+		});
+		return isPending ? 
+			<h1 className='tc'>LOADING</h1>:
+			(
 				<div className="tc">
 					<h1 className='f2'>ROBOFRIENDS</h1>
-					<SearchBox searchChange={this.onSearchChange}/>
+					<SearchBox searchChange={onSearchChange}/>
 					<Scroll>
 						<CardList robots={filteredRobots} />
 					</Scroll>
 				</div>
 			);
-		}
 	}
 }
 
-export default App;
+export default connect(mapStateToProps, mapDispatchToProps)(App);
